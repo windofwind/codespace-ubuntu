3110a6883ed0f0bfa096ce0122903a88325f7798
dev Kim <windofwind@users.noreply.github.com>
2025-05-14 02:36:14 +0000

    fix 2

--- install.sh
+++ install.sh
@@ -17,15 +17,34 @@ git clone https://github.com/zsh-users/zsh-autosuggestions.git $HOME/.oh-my-zsh/
 
 curl -sS https://starship.rs/install.sh | sh -s -- -y
 
+# echo "$(cat $HOME/.zshrc)" | awk '{gsub(/plugins=\(git\)/, "plugins=(git zsh-completions zsh-syntax-highlighting zsh-autosuggestions)")}1' > $HOME/.zshrc.replaced && mv $HOME/.zshrc.replaced $HOME/.zshrc
 
-echo "
-# Starts starship
-eval \"$(starship init zsh)\"
-" >> $HOME/.zshrc
+## OH-MY-ZSH - POWERLEVEL10K SETTINGS ##
+git clone https://github.com/romkatv/powerlevel10k.git $HOME/.oh-my-zsh/custom/themes/powerlevel10k --depth=1
+ln -s $HOME/.oh-my-zsh/custom/themes/powerlevel10k/powerlevel10k.zsh-theme $HOME/.oh-my-zsh/custom/themes/powerlevel10k.zsh-theme
 
+# Uncomment the below to update the oh-my-zsh settings without dotfiles integration
+curl https://raw.githubusercontent.com/justinyoo/devcontainers-dotnet/main/oh-my-zsh/.p10k-with-clock.zsh > $HOME/.p10k-with-clock.zsh
+curl https://raw.githubusercontent.com/justinyoo/devcontainers-dotnet/main/oh-my-zsh/.p10k-without-clock.zsh > $HOME/.p10k-without-clock.zsh
+curl https://raw.githubusercontent.com/justinyoo/devcontainers-dotnet/main/oh-my-zsh/switch-p10k-clock.sh > $HOME/switch-p10k-clock.sh
+chmod +x ~/switch-p10k-clock.sh
+
+cp $HOME/.p10k-with-clock.zsh $HOME/.p10k.zsh
+cp $HOME/.zshrc $HOME/.zshrc.bak
+
+#echo "$(cat $HOME/.zshrc)" | awk '{gsub(/ZSH_THEME=\"devcontainers\"/, "ZSH_THEME=\"codespace\"")}1' > $HOME/.zshrc.replaced && mv $HOME/.zshrc.replaced $HOME/.zshrc
 echo "$(cat $HOME/.zshrc)" | awk '{gsub(/plugins=\(git\)/, "plugins=(git zsh-completions zsh-syntax-highlighting zsh-autosuggestions)")}1' > $HOME/.zshrc.replaced && mv $HOME/.zshrc.replaced $HOME/.zshrc
+
+ZSHRC_ADDITIONS='
+# To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.vi
+#[[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
+
+eval "$(starship init zsh)"
+'
+
+if ! grep -Fxq 'eval "$(starship init zsh)"' "$HOME/.zshrc"; then
+  echo "$ZSHRC_ADDITIONS" >> "$HOME/.zshrc"
+fi
 
 # Change the default shell to zsh
 sudo chsh -s "$ZSH_PATH"
-
-source $ZSH_PATH
